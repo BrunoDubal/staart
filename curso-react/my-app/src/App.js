83c5f2e657596fe258f36cd763487b648172a577
BrunoDubal <bergsonall@outlook.com>
2022-08-20 23:09:01 -0300

    hocks react

--- curso-react/my-app/src/App.js
+++ curso-react/my-app/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import './styles/App.css'
 import './styles/PostForm.css'
 import './styles/Feed.css'
@@ -5,9 +7,10 @@ import './styles/Feed.css'
 import clockIcon from './images/clock.svg'
 import userIcon from './images/user.svg'
 import paperPlaneIcon from './images/paper-plane.svg'
+import emptyFolderIcon from './images/empty-folder.svg'
 
 export default function App() {
-  const posts = [
+  /*const posts = [
     {
       id: Math.random(),
       content: 'Conteudo do post',
@@ -20,20 +23,48 @@ export default function App() {
       userName: 'Usuario 2',
       publishedAt: new Date()
     }
-  ]
+  ];*/
+  const [userName, setUserName] = useState('')
+  const [history, setHistory] = useState('')
+  const [posts, setPosts] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    setPosts([
+      ...posts,
+      {
+        id: Math.random(),
+        content: history,
+        userName,
+        publishedAt: new Date()
+      },
+    ]);
+
+    setHistory('');
+    setUserName('');
+  };
 
   return (
     <div className="wrapper">
       <form
         className="post-form"
-        onSubmit={() => alert('Formulário submetido.')}
+        onSubmit={handleSubmit}
       >
-        <input placeholder="Escreva uma nova história..." />
+        <input 
+          value={history}
+          placeholder="Escreva uma nova história..." 
+          onChange={(event)=> setHistory(event.target.value)}
+        />
 
         <div>
           <img src={userIcon} alt="user" />
 
-          <input placeholder="Digite seu nome..." />
+          <input 
+            value={userName}
+            placeholder="Digite seu nome..." 
+            onChange={(event)=> setUserName(event.target.value)}
+          />
 
           <button type="submit">
             <img src={paperPlaneIcon} />
@@ -43,32 +74,42 @@ export default function App() {
       </form>
 
       <main>
-        <header>
-          <h1>Seu Feed</h1>
-          <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
-        </header>
+        {posts.length === 0 && <div className='feed-status'>
+          <img src={emptyFolderIcon} alt='empty Folder'/>
+          <h1>Não encontramos nada...</h1>
+          <h2>Parece que voce e seus amigos não postaram nada. Comece a escrever uma nova história.</h2>
+        </div>}
+        
+        {posts.length > 0 && 
+        <>
+          <header>
+            <h1>Seu Feed</h1>
+            <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
+          </header>
+          
+          <section className="feed">
+            {posts.map(post => (
+              <article key={post.id}>
+                <p>{post.content}</p>
 
-        <section className="feed">
-          {posts.map(post => (
-            <article key={post.id}>
-              <p>{post.content}</p>
+                <footer>
+                  <div className="user-details">
+                    <img src={userIcon} alt="user" />
+                    <strong>{post.userName}</strong>
+                  </div>
 
-              <footer>
-                <div className="user-details">
-                  <img src={userIcon} alt="user" />
-                  <strong>{post.userName}</strong>
-                </div>
-
-                <div className="time">
-                  <img src={clockIcon} alt="clock" />
-                  <span>
-                    Publicado em {post.publishedAt.toLocaleDateString('pt-br')}
-                  </span>
-                </div>
-              </footer>
-            </article>
-          ))}
-        </section>
+                  <div className="time">
+                    <img src={clockIcon} alt="clock" />
+                    <span>
+                      Publicado em {post.publishedAt.toLocaleDateString('pt-br')}
+                    </span>
+                  </div>
+                </footer>
+              </article>
+            ))}
+          </section>
+        </>
+        }
       </main>
     </div>
   )
